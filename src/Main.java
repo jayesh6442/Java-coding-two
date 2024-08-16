03e3d1e5696b7ceecc2be9e1a0f7456eef6257ad
jayesh <jayeshsarvaiya9913@gamil.com>
2024-08-16 16:16:18 +0530

    add code

---
+++ src/Main.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("hello world!"); this is the method to print the string number and variables
+//        System.out.println(4); this is used for the number printing
+
+//    these are called the comment we use them for the documentation of the code
+//        this are the single line comments
+        /*
+        this are the multi line comment for the long string for the code documentation
+
+        */
+
+//        now we are in the variables
+//        string
+//        int.
+//        float
+//        char/
+//        boolean
+
+
+//        declaration of the variable is give below
+//        type variable_name = variable value
+//        String name = "jayesh";
+//        System.out.println(name);
+//        int age = 22;
+//        System.out.println(age);
+
+//        we have keyword that con used for the variable so that the value of the variable ccant be chagned
+//        like final
+//        final int a = 3;
+//        System.out.println(a);
+//        float a = 3.34f;
+
+//        System.out.println(a);
+//        boolean isMarred = false;
+//        System.out.println(isMarred);
+//        int x = 3;
+//        int y = 3;
+//        int z = 3;
+//        System.out.println(x + y + z);
+//        int x,y,z;
+//        x = y = z= 30;
+//        System.out.println(x+y+z);
+//            we have identifies in java that are used for identify the variables
+//        lets understand with the example
+//        String studentName = "jayesh";
+//        int studentId = 29;
+//        int studentAge  =22;
+//        int studentFee = 0;
+//        char studentGrade =  'A';
+//        System.out.println(studentName);
+//        System.out.println(studentGrade);
+//        System.out.println(studentAge);
+//        System.out.println(studentFee);
+//        System.out.println(studentId);
+//
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        System.out.println(a);
+//    calculating the area of the rectangle
+//        int length = 4;
+//        int width = 6;
+//        int area;
+//        area = length * width;
+//        System.out.println("Length is: " + length);
+//        System.out.println("width is: " + width);
+//        System.out.println("area is: " + area);
+//        java data type
+//        byte
+//        short
+//        int
+//        long
+//        float
+//        double
+//        boolean
+//        char
+
+//        now in this we have special for the numbers
+//        byte
+//        short
+//        int
+//        long
+
+
+//        int type
+//
+//        int age = 22;
+//        System.out.println(age);
+//        byte myBytes = 100;
+//        System.out.println(myBytes);
+//        short myShourt = 4000;
+//        System.out.println(myShourt);
+//        and now we have boolean data type
+
+//        boolean isJavaFun = true;
+//        System.out.println(isJavaFun);
+
+     /*
+        now we are in the character data type
+
+     */
+//     char name = 'a';
+//     take note that char is defined in the single quote and the string are defined in the double quotes
+//        System.out.println(name);
+
+//        java type casting
+//        byte => short => char => int => long => float => double
+//    in this order we can change the data type in java
+//        char change = 'a';
+//        int charToInt  = (int) change;
+//        System.out.println(charToInt);
+//        now we are in the operator section
+
+//        we have arithmetic
+//        assignment
+//        comparison
+//        logical
+//        bitwise
+//        System.out.println(1==1 && 2==2 && 3==3); example of the logical and operation
+//        now we are in the string section
+//
+//        String name = "jayesh";
+//        System.out.println(name.length());
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name.charAt(3));
+//        System.out.println(name.indexOf("j"));
+//        now we are in the string concatenation
+//        String name= "jayesh";
+//        String lastName = "sarvaiya";
+//        System.out.println(name  + " "+lastName);
+
+//        java maths
+//        int max = Math.max(4, 6);
+//        System.out.println(max);
+//        int min = Math.min(2,65);
+//        System.out.println(min);
+//        double sqrt = Math.sqrt(5);
+//        System.out.println(sqrt);
+//        int random=(int)(Math.random() *100);
+//        System.out.println(random);
+
+    }
+}
